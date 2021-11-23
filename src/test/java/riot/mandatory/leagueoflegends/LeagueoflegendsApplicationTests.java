ed228e510c08b4ca4eba1b23cc6ffa84e3c002e1
Kristian097 <73465821+Kristian097@users.noreply.github.com>
2021-11-23 12:15:51 +0100

    Initial commit

---
+++ src/test/java/riot/mandatory/leagueoflegends/LeagueoflegendsApplicationTests.java
@@ -0,0 +1,13 @@
+package riot.mandatory.leagueoflegends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueoflegendsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
