2659d073d44e2832778e8c833535aa9d36fd3c17
KristianHjelmsmarkKEA <kris69j6@stud.kea.dk>
2021-11-23 18:38:59 +0100

    Champions og Summoners CRUD RestController

--- src/main/java/riot/mandatory/leagueoflegends/controllers/Champions.java
+++ src/main/java/riot/mandatory/leagueoflegends/controllers/Champions.java
@@ -2,12 +2,58 @@ package riot.mandatory.leagueoflegends.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import riot.mandatory.leagueoflegends.models.Champion;
+import riot.mandatory.leagueoflegends.models.Summoner;
 import riot.mandatory.leagueoflegends.repositories.ChampionRepository;
+import riot.mandatory.leagueoflegends.repositories.SummonerRepository;
 
 @RestController
 public class Champions {
 
     @Autowired
     ChampionRepository champions;
+
+    @GetMapping("/champions")
+    public Iterable<Champion> getChampion() {
+        return champions.findAll();
+    }
+
+    @GetMapping("/champions/{id}")
+    public Champion getChampion(@PathVariable Long id) {
+        return champions.findById(id).get();
+    }
+
+    @PostMapping("/champions")
+    public Champion addChampion(@RequestBody Champion newChampion) {
+        return champions.save(newChampion);
+    }
+
+    @PutMapping("/champions/{id}")
+    public String updateChampion(@PathVariable Long id, @RequestBody Champion championToUpdate){
+        if(champions.existsById(id)) {
+            championToUpdate.setId(id);
+            champions.save(championToUpdate);
+            return "Champion was updated";
+        } else {
+            return "Champion not found";
+        }
+    }
+
+    @PatchMapping("/champions/{id}")
+    public String patchChampion(@PathVariable Long id, @RequestBody Champion championToUpdate) {
+        return champions.findById(id).map( foundChampion -> {
+            if(championToUpdate.getChampionId() != null) foundChampion.setChampionId(championToUpdate.getChampionId());
+            if(championToUpdate.getChampionName() != null) foundChampion.setChampionName(championToUpdate.getChampionName());
+            if(championToUpdate.getChampionImage() != null) foundChampion.setChampionImage(championToUpdate.getChampionImage());
+            champions.save(foundChampion);
+            return "Champion updated";
+        }).orElse("Champion not found");
+    }
+
+    @DeleteMapping("/champion/{id}")
+    public void deleteChampion(@PathVariable Long id) {
+        champions.deleteById(id);
+    }
+
 }
--- src/main/java/riot/mandatory/leagueoflegends/controllers/Summoners.java
+++ src/main/java/riot/mandatory/leagueoflegends/controllers/Summoners.java
@@ -1,4 +1,59 @@
 package riot.mandatory.leagueoflegends.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import riot.mandatory.leagueoflegends.models.Summoner;
+import riot.mandatory.leagueoflegends.repositories.SummonerRepository;
+
+@RestController
 public class Summoners {
+
+    @Autowired
+    SummonerRepository summoners;
+
+    @GetMapping("/summoners")
+    public Iterable<Summoner> getSummoner() {
+        return summoners.findAll();
+    }
+
+    @GetMapping("/summoners/{id}")
+    public Summoner getSummoner(@PathVariable Long id) {
+        return summoners.findById(id).get();
+    }
+
+    @PostMapping("/summoners")
+    public Summoner addSummoner(@RequestBody Summoner newSummoner) {
+        return summoners.save(newSummoner);
+    }
+
+    @PutMapping("/summoner/{id}")
+    public String updateSummoner(@PathVariable Long id, @RequestBody Summoner summonerToUpdate){
+        if(summoners.existsById(id)) {
+            summonerToUpdate.setId(id);
+            summoners.save(summonerToUpdate);
+            return "Summoner was updated";
+        } else {
+            return "Summoner not found";
+        }
+    }
+
+    @PatchMapping("/summoners/{id}")
+    public String patchSummoner(@PathVariable Long id, @RequestBody Summoner summonerToUpdate) {
+        return summoners.findById(id).map( foundSummoner -> {
+            if(summonerToUpdate.getSummonerId() != null) foundSummoner.setSummonerId(summonerToUpdate.getName());
+            if(summonerToUpdate.getPuuId() != null) foundSummoner.setPuuId(summonerToUpdate.getPuuId());
+            if(summonerToUpdate.getName() != null) foundSummoner.setName(summonerToUpdate.getName());
+            if(summonerToUpdate.getTier() != null) foundSummoner.setTier(summonerToUpdate.getTier());
+            if(summonerToUpdate.getSoloRank() != null) foundSummoner.setSoloRank(summonerToUpdate.getSoloRank());
+            if(summonerToUpdate.getLeaguePoints() != 0) foundSummoner.setLeaguePoints(summonerToUpdate.getLeaguePoints());
+            summoners.save(foundSummoner);
+            return "Summoner updated";
+        }).orElse("Summoner not found");
+    }
+
+    @DeleteMapping("/summoner/{id}")
+    public void deleteSummoner(@PathVariable Long id) {
+        summoners.deleteById(id);
+    }
+
 }
