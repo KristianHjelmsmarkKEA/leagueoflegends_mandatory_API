7cf6ae7d1dcd955623113400fe1fd8c58e2f766e
Lobfohns <ludv1428@stud.kea.dk>
2021-11-23 13:00:49 +0100

    dimser

--- src/main/java/riot/mandatory/leagueoflegends/models/Match.java
+++ src/main/java/riot/mandatory/leagueoflegends/models/Match.java
@@ -1,4 +1,17 @@
 package riot.mandatory.leagueoflegends.models;
 
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Table(name="matches")
+@Entity
 public class Match {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
 }
--- src/main/java/riot/mandatory/leagueoflegends/models/Summoner.java
+++ src/main/java/riot/mandatory/leagueoflegends/models/Summoner.java
@@ -1,4 +1,17 @@
 package riot.mandatory.leagueoflegends.models;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+
+@Data
+@Table(name="summoners")
+@Entity
 public class Summoner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
 }
