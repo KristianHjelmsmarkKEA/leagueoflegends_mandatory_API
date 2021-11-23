5c58682bad3f8be3099257213fceed98341a4408
Lobfohns <ludv1428@stud.kea.dk>
2021-11-23 14:31:19 +0100

    summonerepo fix

--- src/main/java/riot/mandatory/leagueoflegends/repositories/SummonerRepository.java
+++ src/main/java/riot/mandatory/leagueoflegends/repositories/SummonerRepository.java
@@ -1,2 +1,7 @@
-package riot.mandatory.leagueoflegends.repositories;public interface SummonerRepository {
+package riot.mandatory.leagueoflegends.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import riot.mandatory.leagueoflegends.models.Summoner;
+
+public interface SummonerRepository extends JpaRepository<Summoner, Long> {
 }
