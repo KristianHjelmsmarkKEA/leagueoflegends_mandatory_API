3908a0b481dd2a03d2b76833734b5c821324c1b4
Kristian097 <73465821+Kristian097@users.noreply.github.com>
2021-11-25 13:55:00 +0100

    Jscript rettelser forsøg

--- src/main/resources/templates/summoners/summoners.js
+++ src/main/resources/templates/summoners/summoners.js
@@ -1,8 +1,7 @@
 const summonerDiv = document.getElementById("summoner-gallery");
 const riotKey = 'RGAPI-476d9838-d954-4b2a-8a93-13ccdfeece30';
 const summonerAccSearchLink = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key= " + (riotKey);
-const summonerRankSearchLink = "https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/" + id + "?api_key=" + (riotKey);
-let summonerToSave = summonerAccToSave + summonerRankToSave;
+
 
 
 fetch(summonerAccSearchLink)
@@ -11,33 +10,11 @@ fetch(summonerAccSearchLink)
         saveSummonerAccInfo(summoner)
 
     });
-fetch(summonerRankSearchLink)
-    .then(response => response.json())
-    .then(summoner => {
-        saveSummonerRankInfo(summoner)
-    });
-
-function saveSummonerAccInfo(summoner) {
-    let summonerAccToSave = {
-        summonerId: summoner.id,
-        accountId: summoner.accountId,
-        puuId: summoner.puuId,
-        name: summoner.name,
-    };
-}
-
-function saveSummonerRankInfo(summoner) {
-    let summonerRankToSave = {
-        tier: summoner.tier,
-        queueType: summoner.queueType,
-        leaguePoints: summoner.leaguePoints
-    };
-}
 
 function addSummonerInfoToDiv(summoner){
-    const selectSummonerToDiv = document.createElement("div");
-    summonerDiv.appendChild(selectSummonerToDiv);
-    createSummoner(selectSummonerToDiv, summoner);
+    const summonerToDiv = document.createElement("div");
+    summonerDiv.appendChild(summonerToDiv);
+    createSummoner(summonerToDiv, summoner);
 }
 
 function createSummoner(divElement, summoner){
@@ -50,21 +27,31 @@ function createSummoner(divElement, summoner){
     `;
 }
 
-fetch(localurl + "/summoners", {
-    method: "POST",
-    headers: { "Content-type": "application/json" },
-    body: JSON.stringify(summonerToSave)
-}).then(response => {
-    if (response.status === 200) {
-        addSummonerInfoToDiv(summonerToSave);
-    } else {
-        console.log("summoner not created", respone.status);
-    }
-})
-    .catch(error => console.log("network error" + error));
+function saveSummonerAccInfo(summoner) {
+    let summonerAccToSave = {
+        summonerId: summoner.id,
+        accountId: summoner.accountId,
+        puuId: summoner.puuId,
+        name: summoner.name,
+    };
 
-document.getElementById("add-new-summoner-button")
-    .addEventListener("click", saveSummonerAccInfo, saveSummonerRankInfo);
+    fetch(summonerAccSearchLink, {
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify(summonerAccToSave)
+    }).then(response => {
+        if (response.status === 200) {
+            addSummonerInfoToDiv(summoner);
+        } else {
+            console.log("summoner not created", respone.status);
+        }
+    })
+        .catch(error => console.log("network error" + error));
+}
+
+
+document.getElementById("search-for-summoner")
+    .addEventListener("click", saveSummonerAccInfo);
 
 
 
