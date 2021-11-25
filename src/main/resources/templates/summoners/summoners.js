fa2e8bd4e06896fbc694b3fed7b14b060fe9dbcb
Ludvig <ludvig.fohns@gmail.com>
2021-11-25 14:56:29 +0100

    security osv

--- src/main/resources/templates/summoners/summoners.js
+++ src/main/resources/templates/summoners/summoners.js
@@ -1,8 +1,6 @@
 const summonerDiv = document.getElementById("summoner-gallery");
 const riotKey = 'RGAPI-affcec8b-e850-4ebc-91b5-0ee691fd276f';
 const summonerAccSearchLink = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key= " + (riotKey);
-const riotKey = 'RGAPI-476d9838-d954-4b2a-8a93-13ccdfeece30';
-const summonerAccSearchLink = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + name + "?api_key=" + riotKey;
 
 
 
